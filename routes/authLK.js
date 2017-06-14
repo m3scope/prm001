3a331a5cb4e785339313831478bb2f5e966cca50
ityn <ityn8161@gmail.com>
2017-06-14 17:36:49 +0700

    prm001

--- routes/authLK.js
+++ routes/authLK.js
@@ -1,9 +1,31 @@
 /**
  * Created by freez on 01.06.2017.
  */
+var mongoose = require('mongoose');
+
+var config = require('config');
+
+var dbConfig = config.get('dbConfig');
+
+var db = mongoose.connect(dbConfig.uri, dbConfig.options);
+db.connection.on('error', console.error.bind(console, 'connection error:'));
+db.connection.once('open', function () {
+    console.log('Running DB');
+    //Schemas
+    /*var Book = new mongoose.Schema({
+        title: String,
+        author: String,
+        releaseDate: Date
+    });
+
+    //Models
+    var BookModel = mongoose.model('Book', Book);
+    */
+});
 
 var authLK = function(req, res, next) {
     console.log('Middlware');
+    console.log(dbConfig.uri);
     if (true){
         req.user = 'user fking shits';
         next();
