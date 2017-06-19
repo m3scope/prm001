be8b013f26c559a8d19b9cd7647c49caff10147a
ityn <ityn8161@gmail.com>
2017-06-19 17:49:20 +0700

    prm001

--- routes/users_post.js
+++ routes/users_post.js
@@ -3,7 +3,7 @@
  */
 var UserModel = require('../db/models/user');
 
-var rnd = random(1000);
+var rnd = Math.round(Math.random()*1000);
 var newUser = new UserModel({
     catagory: 'amd',
     name_f: 'name'+String(rnd),
@@ -14,7 +14,7 @@ var newUser = new UserModel({
 
 var users_post = function(req, res, next) {
     console.log('Middlware');
-    console.log(dbConfig.uri);
+    //console.log(dbConfig.uri);
     newUser.save(function (err) {
         if(err) throw err;
         console.log('User created!!!');
