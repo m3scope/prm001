e03bc1414da404b77cbe9e943a5ea599af968dc5
ityn <ityn8161@gmail.com>
2017-06-29 09:27:00 +0700

    prm001

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -6,6 +6,9 @@ var config = require('config');
 
 var dbConfig = config.get('dbConfig');
 
+mongoose.Promise = Promise; // Просим Mongoose использовать стандартные Промисы
+mongoose.set('debug', true);  // Просим Mongoose писать все запросы к базе в консоль. Удобно для отладки кода
+
 mongoose.connect(dbConfig.uri);    //, dbConfig.options);
 console.log(dbConfig.uri);
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
--- models/user.js
+++ models/user.js
@@ -2,14 +2,41 @@
  * Created by freez on 09.06.2017.
  */
 var mongoose = require('../libs/mongoose');
-var Schema = mongoose.Schema;
+var crypto = require('crypto'); // модуль node.js для выполнения различных шифровальных операций, в т.ч. для создания хэшей.
 
-var UserSchema   = new Schema({
-    catagory: String,
-    name_f: String,
-    create_at: { type: Date, default: Date.now },
-    update_at: Date,
-    email: String
+var userSchema = new mongoose.Schema({
+    displayName: String,
+    email: {
+        type: String,
+        required: 'Укажите e-mail',
+        unique: 'Такой e-mail уже существует'
+    },
+    passwordHash: String,
+    salt: String
+}, {
+    timestamps: true
 });
 
-module.exports = mongoose.model('User', UserSchema);
+userSchema.virtual('password')
+    .set(function (password) {
+        this._plainPassword = password;
+        if (password) {
+            this.salt = crypto.randomBytes(128).toString('base64');
+            this.passwordHash = crypto.pbkdf2Sync(password, this.salt, 1, 128, 'sha1');
+        } else {
+            this.salt = undefined;
+            this.passwordHash = undefined;
+        }
+    })
+
+    .get(function () {
+        return this._plainPassword;
+    });
+
+userSchema.methods.checkPassword = function (password) {
+    if (!password) return false;
+    if (!this.passwordHash) return false;
+    return crypto.pbkdf2Sync(password, this.salt, 1, 128, 'sha1') == this.passwordHash;
+};
+
+var User = mongoose.model('User', userSchema);
