37a93ebcac2d63c04ac54f8fa33be32a54256525
ityn <ityn8161@gmail.com>
2017-09-18 15:38:01 +0700

    prm001

--- routes/getProfile.js
+++ routes/getProfile.js
@@ -1,12 +1,33 @@
 const loadUser = require("../libs/loadUser");
 
+function formatDate(dt, cb) {
+    const today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth()+1; //January is 0!
+
+    let yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd;
+    }
+    if(mm<10){
+        mm='0'+mm;
+    }
+    cb(dd+'/'+mm+'/'+yyyy);
+}
+
 exports.get = function(req, res){
     loadUser(req.session.user, function (err, user) {
         let LoginRegister = '<b><a href="/profile">Профиль</a></b>';
         if(!req.session.user){
             LoginRegister = '<b><a href="/login">вход</a></b>';
         }
-        res.render('profile', {title: 'Профиль', user: user, LoginRegister: LoginRegister});
+        formatDate(user.createdAt, function(dt){
+            //console.log(user.createdAt);
+            user.createAt = dt;
+            console.log(user.createAt);
+            res.render('profile', {title: 'Профиль', user: user, LoginRegister: LoginRegister});
+        });
+
     });
     //res.render('profile', {title: 'USERS authLK', user: User});
 };
