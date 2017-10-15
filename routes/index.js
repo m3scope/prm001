11a4ad2f32accbdf7f98f1bed6e2c7cf93608952
ityn <ityn8161@gmail.com>
2017-10-16 02:11:54 +0700

    Commit home

--- routes/index.js
+++ routes/index.js
@@ -91,6 +91,33 @@ router.post('/register', (req, res) => {
 
 });
 
+//***********************************************
+
+router.get('/getrec/:id', (req, res) => {
+    //const request = require("request");
+    let rnd = Math.random();
+    const pzm = req.params.id;
+    const http = require('http');
+    const url = "http://blockchain.prizm.space/prizm?requestType=getAccount&account="+pzm+"&random="+rnd;
+    console.log(url);
+
+    http.get(url, function(ress){
+        let body = '';
+
+        ress.on('data', function(chunk){
+            body += chunk;
+        });
+
+        ress.on('end', function(){
+            let fbResponse = JSON.parse(body);
+            console.log("Got a response: ", fbResponse);
+            return res.status(200).send(fbResponse);
+        });
+    }).on('error', function(e){
+        console.log("Got an error: ", e);
+        return res.status(500).send('Внутренняя ошибка!');
+    });
+});
 
 
 //***********************************************
