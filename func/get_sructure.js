079ac250aa320b7ee37ecf43c0df1eef86123caa
ityn <ityn8161@gmail.com>
2017-10-22 03:26:32 +0700

    Commit home

--- func/get_sructure.js
+++ func/get_sructure.js
@@ -31,38 +31,61 @@ function getTransactions(id, num, cb) {
     });
 }
 
-/*
-let awfunc = async function () {
-    const http = require('http');
-    const url = "http://blockchain.prizm.space/prizm?requestType=getBlockchainTransactions&account="+id+"&firstIndex=0&lastIndex=100&random="+rnd;
-    let i=0;
-    while (i<3){
-        let htp = await http.get(url, function(ress){
-            let body = '';
 
-            ress.on('data', function(chunk){
-                body += chunk;
-            });
+function requestAsync(id) {
+  //return new Promise((resolve, reject) => {
+      const rnd = Math.random();
+      const http = require('http');
+      const url = "http://blockchain.prizm.space/prizm?requestType=getBlockchainTransactions&account="+id+"&firstIndex=0&lastIndex=100&random="+rnd;
+      //http://blockchain.prizm.space/prizm?requestType=getAccount&account="+pzm+"&random="+rnd;
+      console.log(url);
 
-            ress.on('end', function(){
-                let fbResponse = JSON.parse(body);
-                //console.log(fbResponse);
-                if (fbResponse.errorCode){
-                    //return cb({status: 500, txt: fbResponse.errorDescription}, num, null)
-                }
-                console.log("Got a response: ", fbResponse.transactions.length);
-                //***********************
-                //      функция разбора транзакций
-                //***********************
-                //return cb(null, num, fbResponse.transactions);
-            });
-        }).on('error', function(e){
-            console.log("Got an error: ", e);
-            //return cb({status: 500, txt: 'Внутренняя ошибка!'}, num, null);
-        });
-    }
-};
-*/
+      http.get(url, function(ress){
+          let body = '';
+
+          ress.on('data', function(chunk){
+              body += chunk;
+          });
+
+          ress.on('end', function(){
+              let fbResponse = JSON.parse(body);
+              //console.log(fbResponse);
+              if (fbResponse.errorCode){
+                  return({status: 500, txt: fbResponse.errorDescription})
+              }
+              console.log("Got a response: ", fbResponse.transactions.length);
+              //***********************
+              //      функция разбора транзакций
+              //***********************
+              return(fbResponse.transactions);
+          });
+      }).on('error', function(e){
+          console.log("Got an error: ", e);
+          if(err) return (err)
+      });
+  //});
+}
+
+async function getURL(_url) {
+  let data, url = _url, num =0, dtaa = [];
+  do {
+    const {body} = await requestAsync(url);
+    data = JSON.parse(body);
+      const dt = data;
+      //console.log(numm, dt[dt.length - 1].senderRS, dt[dt.length - 1].amountNQT, dt[dt.length - 1].recipientRS);
+      dt.forEach((entry, indx) => {
+          if (indx === dt.length - 1) {
+              console.log(indx);
+              num++;
+              url = entry.senderRS;
+              dtaa.push({senderRS: entry.senderRS, amountNQT: entry.amountNQT, recipientRS: entry.recipientRS});
+          }
+      });
+    //url = _url + '&pageToken=' + data.nextPageToken;
+  } while(num<3);
+  return dtaa;
+}
+
 
 function awtfnc(id, dta, num) {
     let idd = id;
@@ -122,13 +145,13 @@ let getStructure = (id, cb) => {
     let idd = id;
     let dta = [];
 
-    async function main() {
-        //let resultat = await awtfnc(idd, dta, num);
-        //console.log('resultat', resultat);
-        return cb(null, await awtfnc(idd, dta, num));
-    }
-    main();
-//return cb(null, dta);
+    // async function main() {
+    //     //let resultat = await awtfnc(idd, dta, num);
+    //     //console.log('resultat', resultat);
+    //     return cb(null, await awtfnc(idd, dta, num));
+    // }
+    // main();
+return cb(null, getURL(idd));
 
 
 };
