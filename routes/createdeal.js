e3c3d254691fc51132f6c3ec1c3c37f3607169e6
ityn <ityn8161@gmail.com>
2018-01-15 16:05:20 +0700

    work

--- routes/createdeal.js
+++ routes/createdeal.js
@@ -42,18 +42,20 @@ exports.post = function(req, res){
     //console.log(req);
     const tUser = req.session.user;
     let crDeals = false;
+    const commission_tax = 0.007;
     let tdeal_amount, tdeal_currency, tprice_amount, tprice_currency;
     loadUser.findID(tUser, function (err, user) {
         let LoginRegister = '<b><a href="/profile">Профиль</a>&nbsp;&nbsp;<a href="/logout">Выход</a></b>';
+
         if(!tUser){
             LoginRegister = '<b><a href="/login">вход</a></b>';
         }
         //console.log(req.body.deal_amount);
 
             if(req.body.class*1){
-                if(user[req.body.price_currency]>=req.body.deal_amount*(req.body.price_amount*1+req.body.price_amount*0.05)){
+                if(user[req.body.price_currency]>=req.body.deal_amount*(req.body.price_amount*1+req.body.price_amount*commission_tax)){
                     //console.log(req.body.deal_amount*(req.body.price_amount*1+req.body.price_amount*0.07));
-                    loadUser.saves(req.session.user, [req.body.price_currency], req.body.deal_amount*(req.body.price_amount*1+req.body.price_amount*0.07),Curr[req.body.price_currency], (err, user)=>{
+                    loadUser.saves(req.session.user, [req.body.price_currency], req.body.deal_amount*(req.body.price_amount*1+req.body.price_amount*commission_tax),Curr[req.body.price_currency], (err, user)=>{
                         "use strict";
                         if(err) res.status(500).send('Внутренняя ошибка!');
                         if(!user){
@@ -64,7 +66,7 @@ exports.post = function(req, res){
                             crDeals = true;
                             if(crDeals) {
                                 let newDeal = new Deal();
-                                const commission_tax = 0.007;
+
                                 newDeal.dealerId = user.id;   //: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },     // Id пользователя создавшего сделку
                                 newDeal.deal_amount = req.body.deal_amount;   //: {type: Number, default: 0},      // количество продаваемой валюты
                                 newDeal.deal_amount_bill = req.body.deal_amount;   //: {type: Number, default: 0},      // количество продаваемой валюты
@@ -75,8 +77,8 @@ exports.post = function(req, res){
                                 newDeal.class = req.body.class * 1;
 
                                 newDeal.commission_tax = commission_tax;
-                                newDeal.commission = Math.round(req.body.price_amount * commission_tax * 10000)/10000;   //: {type: Number, default: 0},     // Сумма комиссии (~5-7%)
-                                newDeal.commission_summ = (Math.round(req.body.price_amount * commission_tax * 10000)/10000) * newDeal.deal_amount;   // сумма коммисии
+                                newDeal.commission = Math.round(req.body.price_amount * commission_tax * 10000000)/10000000;   //: {type: Number, default: 0},     // Сумма комиссии (~5-7%)
+                                newDeal.commission_summ = (Math.round(req.body.price_amount * commission_tax * 10000000)/10000000) * newDeal.deal_amount;   // сумма коммисии
 
                                 newDeal.price = req.body.price_amount * 1;
                                 newDeal.price1 = 1 / req.body.price_amount;
@@ -120,7 +122,7 @@ exports.post = function(req, res){
                             crDeals = true;
                             if(crDeals) {
                                 let newDeal = new Deal();
-                                const commission_tax = 0.007;
+
                                 newDeal.dealerId = user.id;   //: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },     // Id пользователя создавшего сделку
                                 newDeal.deal_amount = req.body.deal_amount;   //: {type: Number, default: 0},      // количество продаваемой валюты
                                 newDeal.deal_amount_bill = req.body.deal_amount;   //: {type: Number, default: 0},      // количество продаваемой валюты
@@ -131,8 +133,8 @@ exports.post = function(req, res){
                                 newDeal.class = req.body.class * 1;
 
                                 newDeal.commission_tax = commission_tax;
-                                newDeal.commission = Math.round(req.body.price_amount * commission_tax * 10000)/10000;   //: {type: Number, default: 0},     // Сумма комиссии (~5-7%)
-                                newDeal.commission_summ = (Math.round(req.body.price_amount * commission_tax * 10000)/10000) * newDeal.deal_amount;   // сумма коммисии
+                                newDeal.commission = Math.round(req.body.price_amount * commission_tax * 10000000)/10000000;   //: {type: Number, default: 0},     // Сумма комиссии (~5-7%)
+                                newDeal.commission_summ = (Math.round(req.body.price_amount * commission_tax * 10000000)/10000000) * newDeal.deal_amount;   // сумма коммисии
 
                                 newDeal.price = req.body.price_amount * 1;
                                 newDeal.price1 = 1 / req.body.price_amount;
