af7c14dc5030aa1a626fca6341f00463a414b429
ityn <ityn8161@gmail.com>
2018-01-16 01:50:09 +0700

    Commit home

--- libs/db_bills.js
+++ libs/db_bills.js
@@ -1,6 +1,5 @@
 //const loadUser = require("loadUser");
 const Deal = require('../models/deal');
-
 const Bill = require('../models/bill');
 const Curr = {
     'currSilver' : 3,
@@ -8,15 +7,66 @@ const Curr = {
     'currPrizm' : 1
 };
 
-//const dealId = Deal.ObjectId("5a532408b1dc980da81bfcc1");
-async function requestAsync(saldo, deal) {
+function cr_Bill(dealID, saldo, deal2Id, cb) {
+    let newBill = new Bill;
 
-    return data;
+
+    newBill.save((err, savedBill)=>{
+        "use strict";
+        if(err) {
+            console.error(err);
+            cb(err, null);
+        }
+        cb(null, savedBill);
+    });
+}
+
+//const dealId = Deal.ObjectId("5a532408b1dc980da81bfcc1");
+async function requestAsync(dealID, saldo, deal2Id) {
+    let data = {};
+    let deal_amount_bill = 0;
+    Deal.findById(deal2Id, (err, deal2) => {
+        "use strict";
+        if(err) console.error(err);
+        if(saldo <= deal2.deal_amount_bill){
+            deal2.deal_amount_bill = deal2.deal_amount_bill - saldo;
+            deal2.save((err)=>{
+                if(err) console.error(err);
+                Deal.findById(dealID, (err, deal)=>{
+                    if(err) console.error(err);
+                    deal.deal_amount_bill = deal.deal_amount_bill - saldo;
+                    deal.save((err)=>{
+                        if(err) console.error(err);
+                        data.saldo = 0;
+                        cr_Bill(dealID, saldo, deal2Id);
+                        return data;
+                    });
+                });
+            });
+        } else {
+            deal_amount_bill = deal2.deal_amount_bill;
+            data.saldo = saldo - deal_amount_bill;
+            deal2.deal_amount_bill = 0;
+            deal2.save((err)=>{
+                if(err) console.error(err);
+                Deal.findById(dealID, (err, deal)=>{
+                    if(err) console.error(err);
+                    deal.deal_amount_bill = deal.deal_amount_bill - deal_amount_bill;
+                    deal.save((err)=>{
+                        if(err) console.error(err);
+                        cr_Bill(dealID, deal_amount_bill, deal2Id);
+                        return data;
+                    });
+                });
+            });
+        }
+    });
+    //return data;
 }
 
 exports.createBillsFromDeal = function (dealId) {
     Deal.findOne({_id: dealId}, function (err, dataDeal) {
-        if(err) console.log(err);
+        if(err) console.error(err);
         Deal.find({
             class: Math.abs(dataDeal.status*1 - 1),
             price_amount: {$lte: dataDeal.price_amount},
@@ -27,17 +77,20 @@ exports.createBillsFromDeal = function (dealId) {
             .exec(function (err, deals) {
                 console.log('------------- DEALS -----------');
                 console.log(deals);
-                if(err) console.log(err);
+                if(err) console.error(err);
                 if(deals.length>0){
                     let num = 0;
-                    async function cr_Bill(amount) {
+                    async function update_Deals(amount) {
                         let saldo = amount;
                         // noinspection JSAnnotator
                         for (let deal of deals){
-                            let data = await requestAsync(saldo, deal);
+                            let data = await requestAsync(dataDeal._id, saldo, deal._id);
+                            saldo = data.saldo;
+                            if(saldo <=0) {break;}
                             num++;
                         }
                     }
+                    update_Deals(dataDeal.deal_amount_bill);
                 }
         });
     });
