15cc198b46e60a5685a111d5b80847e4730d9a97
ityn <ityn8161@gmail.com>
2018-02-19 21:08:07 +0700

    Commit home banks

--- models/bank.js
+++ models/bank.js
@@ -4,15 +4,32 @@ const crypto = require('crypto'); // модуль node.js для выполне�
 
 const bankSchema = new mongoose.Schema({
     dealerId: { type: mongoose.Schema.ObjectId, ref: 'User'},     // Id пользователя владелец кошелька
-    bank_id: {type: Number, default: 0},        // числовой код
-    bank_name: String,                      // Наименование "банка"
+
+    bank_id: {type: Number, default: 0},        // числовой код (0 - PRIZM, 1 - QIWI, 2 - Yandex, 3 - PerfectMoney)
+    bank_name: String,                      // Наименование "банка" ['PRIZM','QIWI','Yandex','PerfectMoney']
     bank_number: String,            // номер счета (кошелька)
-    currency: {type: Number, default: 0},  // Код (число) валюты
-    currency_name: String,                  // Наименование валюты
+
+    currency: {type: Number, default: 0},  // Код (число) валюты (1 - PZM, 2 - USD, 3 - RUR)
+    currency_name: String,                  // Наименование валюты ['','PZM','USD','RUR']
+
     summ_transactions: {type: Number, default: 190000},       // сумма дневных транзакций
     summ_all: {type:Number, default: 50000},                // сумма хранения
+
     summ_trans_current: {type: Number, default: 0},       // сумма дневных транзакций текущая
     summ_all_current: {type:Number, default: 0},                // сумма хранения текущая
+
+    query_in: [
+        {
+            query_id:{ type: mongoose.Schema.ObjectId, ref: 'Query', required: true }
+        }
+    ],
+
+    query_out: [
+        {
+            query_id:{ type: mongoose.Schema.ObjectId, ref: 'Query', required: true }
+        }
+    ]
+
 }, {
     timestamps: true
 });
