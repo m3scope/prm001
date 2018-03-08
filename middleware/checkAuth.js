741057423367feeae23cc22f36fcf986fbe754c6
ityn <ityn8161@gmail.com>
2018-03-09 00:14:18 +0700

    Commit home

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -7,5 +7,8 @@ module.exports = function(req, res, next){
     if(!req.session.user){
         return res.render('login', {title: 'Авторизация'});
     }
+    req.session.reload(function(err) {
+        // session updated
+    });
     next();
 };
