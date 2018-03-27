4a27d2943a65b8bcb6a1e584f201236f872f414d
ityn <ityn8161@gmail.com>
2018-03-27 23:08:58 +0700

    Commit home

--- routes/q_deal.js
+++ routes/q_deal.js
@@ -24,38 +24,77 @@ exports.get = function(req, res){
                         res.render('info', {infoTitle: '<div class="w3-red">Ошибка!</div>', infoText: 'Внутренняя ошибка!', url: '/', title: 'Отмена ордера', user: user, LoginRegister: LoginRegister});
                     } else {
                         if(deal){
-                            deal.status = 3;
-                            deal.save(function (err, sdeal) {
-                                if(err) {
-                                    console.error(err);
-                                    res.render('info', {infoTitle: '<div class="w3-red">Ошибка!</div>', infoText: 'Внутренняя ошибка!', url: '/', title: 'Отмена ордера', user: user, LoginRegister: LoginRegister});
-                                } else {
-                                    if(deal.class == 1){        // покупка
-                                        //const amount = deal.summ_bill
-                                        user[Curr[deal.price_currency][0]] = user[Curr[deal.price_currency][0]] + deal.summ_bill;
-                                        //user[Curr[deal.price_currency][1]] = user[Curr[deal.price_currency][1]] - deal.summ_bill;
-                                        user.save(function (err, u) {
-                                            if(err){
-                                                console.error(err);
-                                                res.render('info', {infoTitle: '<div class="w3-red">Ошибка!</div>', infoText: 'Внутренняя ошибка!', url: '/', title: 'Отмена ордера', user: user, LoginRegister: LoginRegister});
-                                            } else {
-                                                res.render('info', {infoTitle: '<div class="w3-green">Успех!</div>', infoText: 'Ваш ордер отменен!', url: '/profile', title: 'Отмена ордера', user: user, LoginRegister: LoginRegister});
-                                            }
+                            if(deal.status == 1) {
+                                deal.status = 3;
+                                deal.save(function (err, sdeal) {
+                                    if (err) {
+                                        console.error(err);
+                                        res.render('info', {
+                                            infoTitle: '<div class="w3-red">Ошибка!</div>',
+                                            infoText: 'Внутренняя ошибка!',
+                                            url: '/',
+                                            title: 'Отмена ордера',
+                                            user: user,
+                                            LoginRegister: LoginRegister
                                         });
-                                    } else {        //продажа
-                                        user[Curr[deal.deal_currency][0]] = user[Curr[deal.deal_currency][0]] + deal.deal_amount_bill;
-                                        //user[Curr[deal.deal_currency][1]] = user[Curr[deal.deal_currency][1]] - deal.deal_amount_bill;
-                                        user.save(function (err, u) {
-                                            if(err){
-                                                console.error(err);
-                                                res.render('info', {infoTitle: '<div class="w3-red">Ошибка!</div>', infoText: 'Внутренняя ошибка!', url: '/', title: 'Отмена ордера', user: user, LoginRegister: LoginRegister});
-                                            } else {
-                                                res.render('info', {infoTitle: '<div class="w3-green">Успех!</div>', infoText: 'Ваш ордер отменен!', url: '/profile', title: 'Отмена ордера', user: user, LoginRegister: LoginRegister});
-                                            }
-                                        });
+                                    } else {
+                                        if (deal.class == 1) {        // покупка
+                                            //const amount = deal.summ_bill
+                                            user[Curr[deal.price_currency][0]] = user[Curr[deal.price_currency][0]] + deal.summ_bill;
+                                            //user[Curr[deal.price_currency][1]] = user[Curr[deal.price_currency][1]] - deal.summ_bill;
+                                            user.save(function (err, u) {
+                                                if (err) {
+                                                    console.error(err);
+                                                    res.render('info', {
+                                                        infoTitle: '<div class="w3-red">Ошибка!</div>',
+                                                        infoText: 'Внутренняя ошибка!',
+                                                        url: '/',
+                                                        title: 'Отмена ордера',
+                                                        user: user,
+                                                        LoginRegister: LoginRegister
+                                                    });
+                                                } else {
+                                                    res.render('info', {
+                                                        infoTitle: '<div class="w3-green">Успех!</div>',
+                                                        infoText: 'Ваш ордер отменен!',
+                                                        url: '/profile',
+                                                        title: 'Отмена ордера',
+                                                        user: user,
+                                                        LoginRegister: LoginRegister
+                                                    });
+                                                }
+                                            });
+                                        } else {        //продажа
+                                            user[Curr[deal.deal_currency][0]] = user[Curr[deal.deal_currency][0]] + deal.deal_amount_bill;
+                                            //user[Curr[deal.deal_currency][1]] = user[Curr[deal.deal_currency][1]] - deal.deal_amount_bill;
+                                            user.save(function (err, u) {
+                                                if (err) {
+                                                    console.error(err);
+                                                    res.render('info', {
+                                                        infoTitle: '<div class="w3-red">Ошибка!</div>',
+                                                        infoText: 'Внутренняя ошибка!',
+                                                        url: '/',
+                                                        title: 'Отмена ордера',
+                                                        user: user,
+                                                        LoginRegister: LoginRegister
+                                                    });
+                                                } else {
+                                                    res.render('info', {
+                                                        infoTitle: '<div class="w3-green">Успех!</div>',
+                                                        infoText: 'Ваш ордер отменен!',
+                                                        url: '/profile',
+                                                        title: 'Отмена ордера',
+                                                        user: user,
+                                                        LoginRegister: LoginRegister
+                                                    });
+                                                }
+                                            });
+                                        }
                                     }
-                                }
-                            });
+                                });
+                            } else {
+                                res.render('info', {infoTitle: '<div class="w3-red">Ошибка!</div>', infoText: 'Внутренняя ошибка!', url: '/', title: 'Отмена ордера', user: user, LoginRegister: LoginRegister});
+                            }
                         } else {
                             res.render('info', {infoTitle: '<div class="w3-red">Ошибка!</div>', infoText: 'Внутренняя ошибка!', url: '/', title: 'Отмена ордера', user: user, LoginRegister: LoginRegister});
                         }
