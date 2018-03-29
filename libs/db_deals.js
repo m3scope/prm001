cf2a0ce107caeebb593d337ea266efa95aae37f5
ityn <ityn8161@gmail.com>
2018-03-29 21:37:42 +0700

    Commit home

--- libs/db_deals.js
+++ libs/db_deals.js
@@ -7,7 +7,7 @@ exports.getdeals = function (curr1, curr2, cb) {
     if(curr2>3) curr2=3;
     Deal.aggregate([
         {
-            $match:{deal_currency: curr1, price_currency: curr2, status: 0, class: 0}
+            $match:{deal_currency: curr1, price_currency: curr2, status: {$lt:2}, class: 0}
         },
         {
             $group: { _id: "$price", deal_am: { $sum: "$deal_amount_bill" }}
@@ -19,7 +19,7 @@ exports.getdeals = function (curr1, curr2, cb) {
         dt1 = data1;
         Deal.aggregate([
             {
-                $match:{deal_currency: curr1, price_currency: curr2, status: 0, class: 1}
+                $match:{deal_currency: curr1, price_currency: curr2, status: {$lt:2}, class: 1}
             },
             {
                 $group: { _id: "$price", deal_am: { $sum: "$deal_amount_bill" }}
