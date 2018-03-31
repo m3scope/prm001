1ab1e9b6a42aff41f98c6f7160603b2c130b4519
ityn <ityn8161@gmail.com>
2018-03-31 13:36:29 +0700

    Commit home

--- models/bank.js
+++ models/bank.js
@@ -9,7 +9,7 @@ const bankSchema = new mongoose.Schema({
 
     rounds: {type: Number, default: 0},     // позиция в очереди
 
-    bank_cod: {type: Number, default: 0},        // числовой код (0 - PRIZM, 1 - QIWI, 2 - Yandex, 3 - PerfectMoney)
+    bank_cod: {type: Number, default: 0},        // числовой код (0 - PRIZM, 1 - QIWI, 2 - Yandex, 3 - SberBank, 4 - PerfectMoney)
     bank_name: String,                      // Наименование "банка" ['PRIZM','QIWI','Yandex','PerfectMoney']
     bank_number: String,            // номер счета (кошелька)
     bank_publicKey: {type:String, default:''},         // для Призм
