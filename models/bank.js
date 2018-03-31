fc3494355321de625c48ed311d87133595724d64
ityn <ityn8161@gmail.com>
2018-03-31 15:09:25 +0700

    Commit home

--- models/bank.js
+++ models/bank.js
@@ -9,19 +9,25 @@ const bankSchema = new mongoose.Schema({
 
     rounds: {type: Number, default: 0},     // позиция в очереди
 
-    bank_cod: {type: Number, default: 0},        // числовой код (0 - PRIZM, 1 - QIWI, 2 - Yandex, 3 - SberBank, 4 - PerfectMoney)
-    bank_name: String,                      // Наименование "банка" ['PRIZM','QIWI','Yandex','PerfectMoney']
+    bank_cod: {type: Number, default: 99},        // числовой код (0 - PRIZM, 1 - QIWI, 2 - Yandex, 3 - SberBank, 4 - PerfectMoney)
+    bank_name: String,                      // Наименование "банка" ['PRIZM','QIWI','Yandex','SberBank','PerfectMoney']
     bank_number: String,            // номер счета (кошелька)
     bank_publicKey: {type:String, default:''},         // для Призм
 
     currency: {type: Number, default: 0},  // Код (число) валюты (1 - PZM, 2 - USD, 3 - RUR)
     currency_name: String,                  // Наименование валюты ['','PZM','USD','RUR']
 
-    summ_transactions: {type: Number, default: 0},       // сумма дневных транзакций
-    summ_all: {type:Number, default: 0},                // сумма хранения
+    summ_transactions: {type: Number, default: 190000},       // сумма дневных транзакций
+    summ_all: {type:Number, default: 50000},                // максимальная сумма хранения
 
-    summ_trans_current: {type: Number, default: 190000},       // сумма дневных транзакций текущая
-    summ_all_current: {type:Number, default: 50000},                // сумма хранения текущая
+    summ_trans_current: {type: Number, default: 0},       // сумма дневных транзакций текущая
+    summ_all_current: {type:Number, default: 0},                // сумма хранения текущая
+
+    summ_trans_day: {type: Number, default: 0},       // сумма дневных транзакций текущая
+    summ_all_day: {type:Number, default: 0},
+
+    summ_trans_month: {type: Number, default: 0},       // сумма дневных транзакций текущая
+    summ_all_month: {type:Number, default: 0},
 
     summ_rez: {type: Number, default: 0},
 
