99150e85b4be83da3aa92cc775cf6645ba4dc256
ityn <ityn8161@gmail.com>
2018-04-10 23:22:57 +0700

    Commit home

--- routes/responseQuery.js
+++ routes/responseQuery.js
@@ -153,7 +153,7 @@ exports.get = function (req, res, next) {
                             break;
                         case 'cancelamd':
                             if(amd.indexOf(user._id) > -1) {
-                                Query.findById({_id: params[0]}, function (err, qq) {
+                                Query.findOne({_id: params[0]}, function (err, qq) {
                                     if (err) console.error(err);
                                     if (qq) {
                                         //console.log(qq);
@@ -187,40 +187,44 @@ exports.get = function (req, res, next) {
                             break;
                         case 'execut':
                             // ********** execut   // добавление на счет
-                            Query.findOne({_id: params[0], dealerId: user._id}, function (err, qq) {
-                                if (err) console.error(err);
-                                if (qq) {
-                                    //console.log(qq);
-                                    if (qq.status == 1) {
-                                        res.render('executquery', {
-                                            qq: qq,
-                                            title: 'Подтвердить ЗАПРОС',
-                                            user: user,
-                                            LoginRegister: LoginRegister
-                                        });
+                            if(amd.indexOf(user._id) > -1) {
+                                Query.findOne({_id: params[0]}, function (err, qq) {
+                                    if (err) console.error(err);
+                                    if (qq) {
+                                        //console.log(qq);
+                                        if (qq.status == 1) {
+                                            res.render('executquery', {
+                                                qq: qq,
+                                                title: 'Подтвердить ЗАПРОС',
+                                                user: user,
+                                                LoginRegister: LoginRegister
+                                            });
+                                        } else {
+                                            res.render('info', {
+                                                infoTitle: '<div class="w3-red">Ошибка!</div>',
+                                                infoText: 'Операция не выполнена!',
+                                                url: '/profile',
+                                                title: 'Статус запроса не поддерживается!',
+                                                user: user,
+                                                LoginRegister: LoginRegister
+                                            });
+                                            //res.redirect('/logout');
+                                        }
                                     } else {
                                         res.render('info', {
                                             infoTitle: '<div class="w3-red">Ошибка!</div>',
                                             infoText: 'Операция не выполнена!',
                                             url: '/profile',
-                                            title: 'Статус запроса не поддерживается!',
+                                            title: 'Запрос удален!',
                                             user: user,
                                             LoginRegister: LoginRegister
                                         });
                                         //res.redirect('/logout');
                                     }
-                                } else {
-                                    res.render('info', {
-                                        infoTitle: '<div class="w3-red">Ошибка!</div>',
-                                        infoText: 'Операция не выполнена!',
-                                        url: '/profile',
-                                        title: 'Запрос удален!',
-                                        user: user,
-                                        LoginRegister: LoginRegister
-                                    });
-                                    //res.redirect('/logout');
-                                }
-                            });
+                                });
+                            } else {
+                                res.redirect('/logout');
+                            }
                             break;
                         case 'cancelexec':
 
@@ -299,7 +303,7 @@ exports.post = function (req, res, next) {
                     break;
                 case 'cancelamd':
                     if(amd.indexOf(user._id) > -1){
-                        Query.findById({_id:params[0]}, function (err, qq) {
+                        Query.findOne({_id:params[0]}, function (err, qq) {
                             if(err) console.error(err);
                             if(qq){
                                 //console.log(qq);
@@ -327,69 +331,108 @@ exports.post = function (req, res, next) {
                     break;
                 case 'execut':
                     // ********** execut   // добавление на счет
-                    Query.findOne({_id:params[0], dealerId:user._id}, function (err, qq) {
-                        if(err) console.error(err);
-                        if(qq){
-                            //console.log(qq);
-                            if(qq.status == 1){
-                                qq.status = 3;
-                                qq.save(function (err, qqsaved) {
-                                    if(err) {
-                                        console.error(err);
-                                        res.redirect('/logout');
-                                    } else {
-                                        Userr.findById(qqsaved.userId,function (err, userr) {
-                                            if(err) {
-                                                console.error(err);
-                                                res.render('info', {infoTitle: '<div class="w3-red">Ошибка!</div>', infoText: 'User not found! UID: '+qqsaved.UID, url: '/profile', title: 'Запрос подтвержден', user: user, LoginRegister: LoginRegister});
-                                            } else {
-                                                if(userr){
-                                                    if(qqsaved.class == 1){     // пополнение баланса
-                                                        userr[qqsaved.currency_name] = Number(userr[qqsaved.currency_name])+Number(qqsaved.amount)-Number(qqsaved.commission_summ);
-                                                        userr.save();
-                                                        //******** СПИСАНИЕ КОМИССИИ
-                                                        let newTrans3 = new TransactionQuery;
-                                                        newTrans3.sort = 3;
-                                                        newTrans3.sortName = 'списание комиссии';
-                                                        newTrans3.queryId = qqsaved._id;
-                                                        newTrans3.userId = qqsaved.userId;
-                                                        newTrans3.currency = qqsaved.currency;
-                                                        newTrans3.amount = qqsaved.commission_summ;
-                                                        newTrans3.up_down = true;
-                                                        newTrans3.save();
-                                                    } else {
-                                                        //******** СПИСАНИЕ КОМИССИИ
-                                                        let newTrans3 = new TransactionQuery;
-                                                        newTrans3.sort = 3;
-                                                        newTrans3.sortName = 'списание комиссии';
-                                                        newTrans3.queryId = qqsaved._id;
-                                                        newTrans3.userId = qqsaved.userId;
-                                                        newTrans3.currency = qqsaved.currency;
-                                                        newTrans3.amount = qqsaved.commission_summ;
-                                                        newTrans3.up_down = false;
-                                                        newTrans3.save();
-                                                    }
-                                                    res.render('info', {infoTitle: '<div class="w3-green">Успех!</div>', infoText: 'Операция успешно выполнена!', url: '/profile', title: 'Запрос подтвержден', user: user, LoginRegister: LoginRegister});
+                    if(amd.indexOf(user._id) > -1) {
+                        Query.findOne({_id: params[0]}, function (err, qq) {
+                            if (err) console.error(err);
+                            if (qq) {
+                                //console.log(qq);
+                                if (qq.status == 1) {
+                                    qq.status = 3;
+                                    qq.save(function (err, qqsaved) {
+                                        if (err) {
+                                            console.error(err);
+                                            res.redirect('/logout');
+                                        } else {
+                                            Userr.findById(qqsaved.userId, function (err, userr) {
+                                                if (err) {
+                                                    console.error(err);
+                                                    res.render('info', {
+                                                        infoTitle: '<div class="w3-red">Ошибка!</div>',
+                                                        infoText: 'User not found! UID: ' + qqsaved.UID,
+                                                        url: '/profile',
+                                                        title: 'Запрос подтвержден',
+                                                        user: user,
+                                                        LoginRegister: LoginRegister
+                                                    });
                                                 } else {
-                                                    console.error('User not found: '+qqsaved.UID);
-                                                    res.render('info', {infoTitle: '<div class="w3-red">Ошибка!</div>', infoText: 'User not found! UID: '+qqsaved.UID, url: '/profile', title: 'Запрос подтвержден', user: user, LoginRegister: LoginRegister});
+                                                    if (userr) {
+                                                        if (qqsaved.class == 1) {     // пополнение баланса
+                                                            userr[qqsaved.currency_name] = Number(userr[qqsaved.currency_name]) + Number(qqsaved.amount) - Number(qqsaved.commission_summ);
+                                                            userr.save();
+                                                            //******** СПИСАНИЕ КОМИССИИ
+                                                            let newTrans3 = new TransactionQuery;
+                                                            newTrans3.sort = 3;
+                                                            newTrans3.sortName = 'списание комиссии';
+                                                            newTrans3.queryId = qqsaved._id;
+                                                            newTrans3.userId = qqsaved.userId;
+                                                            newTrans3.currency = qqsaved.currency;
+                                                            newTrans3.amount = qqsaved.commission_summ;
+                                                            newTrans3.up_down = true;
+                                                            newTrans3.save();
+                                                        } else {
+                                                            //******** СПИСАНИЕ КОМИССИИ
+                                                            let newTrans3 = new TransactionQuery;
+                                                            newTrans3.sort = 3;
+                                                            newTrans3.sortName = 'списание комиссии';
+                                                            newTrans3.queryId = qqsaved._id;
+                                                            newTrans3.userId = qqsaved.userId;
+                                                            newTrans3.currency = qqsaved.currency;
+                                                            newTrans3.amount = qqsaved.commission_summ;
+                                                            newTrans3.up_down = false;
+                                                            newTrans3.save();
+                                                        }
+                                                        res.render('info', {
+                                                            infoTitle: '<div class="w3-green">Успех!</div>',
+                                                            infoText: 'Операция успешно выполнена!',
+                                                            url: '/profile',
+                                                            title: 'Запрос подтвержден',
+                                                            user: user,
+                                                            LoginRegister: LoginRegister
+                                                        });
+                                                    } else {
+                                                        console.error('User not found: ' + qqsaved.UID);
+                                                        res.render('info', {
+                                                            infoTitle: '<div class="w3-red">Ошибка!</div>',
+                                                            infoText: 'User not found! UID: ' + qqsaved.UID,
+                                                            url: '/profile',
+                                                            title: 'Запрос подтвержден',
+                                                            user: user,
+                                                            LoginRegister: LoginRegister
+                                                        });
+                                                    }
                                                 }
-                                            }
-                                        });
-                                        //res.render('info', {infoTitle: '<div class="w3-green">Успех!</div>', infoText: 'Операция успешно выполнена!', url: '/profile', title: 'Запрос подтвержден', user: user, LoginRegister: LoginRegister});
-                                    }
+                                            });
+                                            //res.render('info', {infoTitle: '<div class="w3-green">Успех!</div>', infoText: 'Операция успешно выполнена!', url: '/profile', title: 'Запрос подтвержден', user: user, LoginRegister: LoginRegister});
+                                        }
 
-                                });
-                                //res.render('info', {infoTitle: '<div class="w3-green">Успех!</div>', infoText: 'Операция успешно выполнена!', url: '/profile', title: 'Запрос подтвержден', user: user, LoginRegister: LoginRegister});
+                                    });
+                                    //res.render('info', {infoTitle: '<div class="w3-green">Успех!</div>', infoText: 'Операция успешно выполнена!', url: '/profile', title: 'Запрос подтвержден', user: user, LoginRegister: LoginRegister});
+                                } else {
+                                    res.render('info', {
+                                        infoTitle: '<div class="w3-red">Ошибка!</div>',
+                                        infoText: 'Операция не выполнена!',
+                                        url: '/profile',
+                                        title: 'Статус запроса не поддерживается!',
+                                        user: user,
+                                        LoginRegister: LoginRegister
+                                    });
+                                    //res.redirect('/logout');
+                                }
                             } else {
-                                res.render('info', {infoTitle: '<div class="w3-red">Ошибка!</div>', infoText: 'Операция не выполнена!', url: '/profile', title: 'Статус запроса не поддерживается!', user: user, LoginRegister: LoginRegister});
+                                res.render('info', {
+                                    infoTitle: '<div class="w3-red">Ошибка!</div>',
+                                    infoText: 'Операция не выполнена!',
+                                    url: '/profile',
+                                    title: 'Запрос удален!',
+                                    user: user,
+                                    LoginRegister: LoginRegister
+                                });
                                 //res.redirect('/logout');
                             }
-                        } else {
-                            res.render('info', {infoTitle: '<div class="w3-red">Ошибка!</div>', infoText: 'Операция не выполнена!', url: '/profile', title: 'Запрос удален!', user: user, LoginRegister: LoginRegister});
-                            //res.redirect('/logout');
-                        }
-                    });
+                        });
+                    } else {
+                        res.redirect('/logout');
+                    }
                     break;
                 case 'canclexec':
 
