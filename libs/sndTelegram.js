b619a23ec6155a91e59f427a4f7094471b52e013
ityn <ityn8161@gmail.com>
2018-04-19 14:02:41 +0700

    Commit home

---
+++ libs/sndTelegram.js
@@ -0,0 +1,4 @@
+// 'https://api.telegram.org/bot565038247:AAG2sILliJR_bx8pM9jO8QcNjqWME4Pt-lo/sendMessage?chat_id=@prizmexamd&text=hello345'
+
+const chat_id = 'chat_id=@prizmexamd';
+let txtMessage = '';
