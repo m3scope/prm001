32935db7adc55ee67ceb6fef4965232e0a6d5567
ityn <ityn8161@gmail.com>
2018-05-14 22:09:05 +0700

    Commit home

--- routes/responseQuery.js
+++ routes/responseQuery.js
@@ -469,8 +469,7 @@ exports.post = function (req, res, next) {
                                     if(Number(qq.class) < 1){   //отмена вывода средств
                                         qq.status = 5;
                                         qq.dateCancel = Date.now();
-                                        if(req.body.comments != '') qq.comments = req.body.comments;
-
+                                        qq.comments = qq.comments + ' / '+req.body.comments;
                                         Userr.findOne({_id:qq.userId}).exec(function (err, usrr) {
                                             if(usrr){
                                                 if(!req.body.no_balance) {
@@ -510,7 +509,7 @@ exports.post = function (req, res, next) {
                                     } else {        // ОТМЕНА ПОПОЛНЕНИЯ БАЛАНСА
                                         qq.status = 5;
                                         qq.dateCancel = Date.now();
-                                        if(req.body.comments != '') qq.comments = req.body.comments;
+                                        qq.comments = qq.comments + ' / '+req.body.comments;
                                         Bank.findById(qq.bankId, function (err, bank) {
                                             if (err) {
                                                 console.error(err);
