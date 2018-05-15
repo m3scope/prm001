1e9358ddcfe0204ef92cb15d91c1f32f083e133d
ityn <ityn8161@gmail.com>
2018-05-15 16:01:27 +0700

    work

--- routes/a_buhs.js
+++ routes/a_buhs.js
@@ -18,6 +18,7 @@ exports.get = function(req, res) {
             res.render('amd_index', {
                 inc: {f: 'a_querys'},
                 title: 'Пользователи',
+                dateRange: {dateAt: dateAt, dateTo: dateTo},
                 querys: querys,
                 dealerId:req.session.user,
                 LoginRegister: 'LoginRegister'
@@ -25,12 +26,14 @@ exports.get = function(req, res) {
             });
 
         } else {
+
             let dateAt = new Date();
             dateAt.setDate(1);
             dateAt.setHours(0);
             dateAt.setMinutes(0);
             dateAt.setSeconds(0);
             dateAt.setMilliseconds(0);
+            const dateTo = new Date();
             Transaction.aggregate(
                 [
                     {
@@ -78,6 +81,7 @@ exports.get = function(req, res) {
                                     res.render('amd_index', {
                                         inc: {f: 'a_buhs'},
                                         title: 'Бухгалтерия',
+                                        dateRange: {dateAt: dateAt, dateTo: dateTo},
                                         aggrTrans: aggrTrans,
                                         aggrQuery: aggrQuery,
                                         userId:req.session.user,
@@ -101,24 +105,81 @@ exports.post = function(req, res) {
     if(!req.session.user){
         return res.render('login', {title: 'Авторизация'});
     } else {
-        const filtr = [{},{status:{$lt:2}},{dealerId:req.session.user},{dealerId:req.session.user,status:{$lt:2}}];
-        const lm = (!req.body.limit || Number(req.body.limit) <= 0) ? 20:Number(req.body.limit);
 
+        let dateAt = new Date(req.body.dateAt);
+        //dateAt.setDate(1);
+        dateAt.setHours(0);
+        dateAt.setMinutes(0);
+        dateAt.setSeconds(0);
+        dateAt.setMilliseconds(0);
 
-        Query.find(filtr[Number(req.body.filter)]).limit(lm).sort({createdAt: -1}).exec(function (err, querys) {
-            if(err){
-                console.error(err);
-                res.redirect('/amd/users');
-            } else {
-                res.render('amd_index', {
-                    inc: {f: 'a_querys'},
-                    title: 'Пользователи',
-                    querys: querys,
-                    dealerId:req.session.user,
-                    LoginRegister: 'LoginRegister'
+        let dateTo = new Date(req.body.dateTo);
+        dateTo.setHours(23);
+        dateTo.setMinutes(59);
+        dateTo.setSeconds(59);
+        dateTo.setMilliseconds(0);
+        //console.log(dateAt);
+        Transaction.aggregate(
+            [
+                {
+                    "$match" : {
+                        "sort" : 3.0,
+                        "createdAt": {$gte: dateAt, $lte: dateTo}
+                    }
+                },
+                {
+                    "$group" : {
+                        "_id" : "$currency",
+                        "amount" : {
+                            "$sum" : "$amount"
+                        }
+                    }
+                }
+            ],function (err, aggrTrans) {
+                if(err){
+                    console.error(err);
+                    res.redirect('/amd/users');
+                } else {
 
-                });
+                    Query.aggregate(
+                        [
+                            {
+                                "$match" : {
+                                    "status" : 3.0,
+                                    "dateExec": {$gte: dateAt, $lte: dateTo}
+                                }
+                            },
+                            {
+                                "$group" : {
+                                    "_id" : "$currency",
+                                    "amount" : {
+                                        "$sum" : "$commission_summ"
+                                    }
+                                }
+                            }
+                        ],function (err, aggrQuery) {
+                            if(err){
+                                console.error(err);
+                                res.redirect('/amd/users');
+                            } else {
+                                console.log(aggrQuery);
+                                res.render('amd_index', {
+                                    inc: {f: 'a_buhs'},
+                                    dateRange: {dateAt: dateAt, dateTo: dateTo},
+                                    title: 'Бухгалтерия',
+                                    aggrTrans: aggrTrans,
+                                    aggrQuery: aggrQuery,
+                                    userId:req.session.user,
+                                    LoginRegister: 'LoginRegister'
+
+                                });
+                            }
+                        }
+                    );
+
+                }
+
             }
-        });
+        );
     }
 };
