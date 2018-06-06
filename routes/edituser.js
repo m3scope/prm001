21d2e0689bbbdf49aa6a4a377565bae93730791c
ityn <ityn8161@gmail.com>
2018-06-06 21:28:47 +0700

    Commit home

--- routes/edituser.js
+++ routes/edituser.js
@@ -4,7 +4,12 @@ const http = require('http');
 
 const fs = require('fs');
 
-const  badAddress = ['PRIZM-CWX6-9DS3-YVBS-AGDVS','PRIZM-C4J9-NRRL-EZDY-PPUZ3'];
+const  badAddress = [
+    'PRIZM-CWX6-9DS3-YVBS-AGDVS',
+    'PRIZM-C4J9-NRRL-EZDY-PPUZ3',
+    'PRIZM-SKXY-62EZ-AAEC-Z64Y4',
+    'PRIZM-YY5C-4AJF-P7E2-R8LY4'
+];
 
 exports.get = function (req, res, next) {
     res.redirect('/');
